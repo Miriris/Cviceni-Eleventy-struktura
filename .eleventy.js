bdf1e7e5827897a7e24b3440e3c6915ca316ad92
Filip Chalupa <chalupa.filip@gmail.com>
2021-10-26 13:27:28 +0200

    Improve sass build

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,9 @@
-const pluginSass = require('eleventy-plugin-sass')
-
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('src/obrazky')
 
-	eleventyConfig.addPlugin(pluginSass, {
-		watch: ['src/styly/index.scss'],
+	eleventyConfig.addWatchTarget('dist/*.css')
+	eleventyConfig.setBrowserSyncConfig({
+		files: ['dist/*.css'],
 	})
 
 	return {
